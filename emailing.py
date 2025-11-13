635608cd20cb72ecb2e5a6b409e261774758d24a
Anassource <anassource@gmail.com>
2025-11-13 17:15:10 +0530

    Threading in Python

--- emailing.py
+++ emailing.py
@@ -5,6 +5,7 @@ PASSWORD = "wbtspodfgqomgadn"
 SENDER = "app8flask@gmail.com"
 RECEIVER = "app8flask@gmail.com"
 def send_email(image_path):
+    print("send_email function started")
     email_message = EmailMessage()
     email_message["Subject"] = "New Customer showed up!"
     email_message.set_content("Hey, we just saw a new customer")
@@ -19,6 +20,7 @@ def send_email(image_path):
     gmail.login(SENDER, PASSWORD)
     gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
     gmail.quit()
+    print("send_email function ended")
 
 if __name__ == "__main__":
     send_email(image_path="images/3.png")
